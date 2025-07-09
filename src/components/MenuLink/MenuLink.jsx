7bf3fbafec41febc74f68928c5297f73abc78d17
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-07-09 10:47:20 -0300

    Refactor MenuLink component to use NavLink

--- src/components/MenuLink/MenuLink.jsx
+++ src/components/MenuLink/MenuLink.jsx
@@ -1,19 +1,17 @@
 import styles from "./MenuLink.module.css";
-import { useLocation } from "react-router";
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 
 function MenuLink({ children, to }) {
-  const location = useLocation();
 
   return (
-    <Link
-      className={`${styles["menu-link"]} ${
-        location.pathname === to ? styles["menu-link--active"] : ""
-      }`}
+    <NavLink
+      className={({ isActive }) =>
+        `${styles.link} ${isActive ? styles.active : ""}`
+      }
       to={to}
     >
       {children}
-    </Link>
+    </NavLink>
   );
 }
 
