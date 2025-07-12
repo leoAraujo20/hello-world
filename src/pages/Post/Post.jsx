1921a53ace1b37aa650a7b882fffdb1bab39010d
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-07-12 10:48:46 -0300

    integrate react-markdown for post content rendering

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,6 +1,8 @@
+import styles from "./Post.module.css";
 import { useParams } from "react-router";
 import posts from "../../assets/json/posts.json"
 import PostModel from "../../components/PostModel/PostModel";
+import Markdown from 'react-markdown'
 
 function Post() {
   let { id } = useParams();
@@ -9,7 +11,12 @@ function Post() {
 
   return (
     <PostModel coverPhoto={`/posts/${post.id}/capa.png`} title={post.title}>
-      {post.text}
+      <div className={styles["post-markdown-container"]}>
+        <Markdown>
+          {post.text}
+        </Markdown>
+      </div>
+        
     </PostModel>
   )
 }
